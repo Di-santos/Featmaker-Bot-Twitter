8fd6be8b92afc7ed5e9ec9773a6df95a13a3adf8
Di-santos <dio.silveira07@gmail.com>
2021-01-17 10:24:12 -0300

    v1.0.0
    - Feats configurados

--- bot.js
+++ bot.js
@@ -1,4 +1,10 @@
 // +++ Feat Bot +++
+/*
+    Bot API:    Bot.método('rotaAPI',{
+                        parâmetros
+                    },
+                    Callback Function{})
+*/
 
 // +++ Login configs +++
 const twit = require('twit');
@@ -11,49 +17,77 @@ const Bot = new twit({
     timeout_ms:60*1000
 })
 
-// +++ Imports +++
+// +++ Imports e Variáveis+++
 const musicas = require('./musicas.json')
 const random = require('random-item');
 const scheduler = require('node-schedule')
-
+let artistas = ["Froid", "Duzz", "Xamã", "Dfideliz", "Mano Brown", "MC Brinquedo"]
+let artista1 = ""
+let artista2 = ""
 let stream = Bot.stream('user')
-let artista1 = "Dfideliz"
-let artista2 = "Xamã"
+// +++ Ações programadas do Bot +++ 
 
-// +++ Ações do Bot +++
-//Bot.{método}('{evento}', mensagem, callback)
-Bot.post('statuses/update', {
-    status: makeFeat(artista1, artista2)
-},
-function (error, data, response){
-    if (error){
-        console.log(data)
+// Feats
+scheduler.scheduleJob({hour: 00, minute: 00 }, ()=>{
+    artista1 = random(artistas)
+    artista2 = random(artistas)
+
+    while (artista1 == artista2){
+        artista2 = random(artistas)
     }
-    else{
-        console.log("Tweet Publicado!)
-    }
+    
+    Bot.post('statuses/update', {
+        status: makeFeat(artista1, artista2)
+    },
+    function (error, data, response){
+        if (error){
+            console.log(data)
+        }
+        else{
+            console.log("Feat Publicado!")
+        }
+    })
+    
+    stream.on('tweet', )
 })
 
+// Singles
+
 // +++ Funções +++
-
-// Recebe os artistas e retorna um feat entre os mesmos
-function makeFeat(artista1, artista2){
-
-    let titulo = `Feat entre ${artista1} e ${artista2}:\n`
+function makeRefrao(artista1, artista2) {
+    let titulo = `Feat entre ${artista1} e ${artista2}:\n\nRefrão:\n\n`
     let versosArtista1 = musicas.find(versos => versos.artista == artista1)
     let versosArtista2 = musicas.find(versos => versos.artista == artista2)
     let estrofeArtista1 = []
     let estrofeArtista2 = []
 
-    for (let i=0; i < 3; i++){
+    for (let i=0; i < 2; i++){
         estrofeArtista1.push(random(versosArtista1.versos))
         estrofeArtista2.push(random(versosArtista2.versos))
     }
+
     let feat = titulo.concat(estrofeArtista1, estrofeArtista2)
-    return feat
+    return feat.replace(/,/g , '')
 }
 
-// Recebe um artista e retorna uma música gerada aleatoriamente
+function makeFeat(artista1, artista2){
+
+    let titulo = `Feat entre ${artista1} e ${artista2}:\n\n`
+    let versosArtista1 = musicas.find(versos => versos.artista == artista1)
+    let versosArtista2 = musicas.find(versos => versos.artista == artista2)
+    let estrofeArtista1 = []
+    let estrofeArtista2 = []
+
+    for (let i=0; i < 2; i++){
+        estrofeArtista1.push(random(versosArtista1.versos))
+        estrofeArtista2.push(random(versosArtista2.versos))
+    }
+
+    let feat = titulo.concat(estrofeArtista1, estrofeArtista2)
+    return feat.replace(/,/g , '')
+}
+
 function makeSong(artista){
-
+    
 }
+
