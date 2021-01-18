c9995111f3e2db5014b7e0208943c9e95a89df54
Di-santos <dio.silveira07@gmail.com>
2021-01-18 12:22:38 -0300

    v1.1.1
    -Adição de cantores

--- bot.js
+++ bot.js
@@ -24,13 +24,14 @@ const random = require('random-item');
 const fs = require( 'fs' );
 const path = require( 'path' );
 
-let artistas = ["Froid", "Duzz", "Xamã", "Dfideliz", "Mano Brown", "MC Brinquedo", "Pabllo Vittar", "Chico Buarque"]
+let artistas = ["Froid", "Duzz", "Xamã", "Dfideliz", "Mano Brown", "MC Brinquedo", "Pabllo Vittar",
+                "Chico Buarque", "Vitão", "Amado Batista"]
 
 
 // +++ Ações programadas do Bot +++ 
 
-// Feats
-scheduler.scheduleJob({hour: 10, minute: 00 }, ()=>{
+// Feats ( 9:00 // 14:00 // 18:00 // 20:00 )
+scheduler.scheduleJob({hour: 12, minute: 00 }, ()=>{
 
     let artista1 = ""
     let artista2 = ""
@@ -102,7 +103,7 @@ scheduler.scheduleJob({hour: 10, minute: 00 }, ()=>{
     })
 })
 
-scheduler.scheduleJob({hour: 15, minute: 00 }, ()=>{
+scheduler.scheduleJob({hour: 17, minute: 00 }, ()=>{
 
     let artista1 = ""
     let artista2 = ""
@@ -174,7 +175,79 @@ scheduler.scheduleJob({hour: 15, minute: 00 }, ()=>{
     })
 })
 
-scheduler.scheduleJob({hour: 18, minute: 00 }, ()=>{
+scheduler.scheduleJob({hour: 21, minute: 00 }, ()=>{
+
+    let artista1 = ""
+    let artista2 = ""
+    let ids = []
+
+    // Seleção dos Artistas
+    artista1 = random(artistas)
+    artista2 = random(artistas)
+
+    while (artista1 == artista2){
+        artista2 = random(artistas)
+    }
+    
+    // Upload das fotos dos artistas
+    fs.readdir( __dirname + '/images', function( err, files ){
+        let cantor1 = musicas.find(versos => versos.artista == artista1)
+        const imagePath1 = path.join( __dirname, '/images/' + `${cantor1.foto}`)
+        let b64content1 = fs.readFileSync( imagePath1, { encoding: 'base64' } );
+    
+        
+        Bot.post( 'media/upload', {
+            media_data: b64content1
+        },
+        ( err, data, response ) => {
+            
+            if ( err ){
+                console.log( 'error:', err );
+            }
+            
+            else{
+    
+                console.log("Primeira Imagem Upada")
+                ids.push(data.media_id_string);
+    
+                let cantor2 = musicas.find(versos => versos.artista == artista2)
+                const imagePath2 = path.join( __dirname, '/images/' + `${cantor2.foto}`)
+                let b64content2 = fs.readFileSync( imagePath2, { encoding: 'base64' } );
+    
+                Bot.post( 'media/upload', {
+                    media_data: b64content2
+                },
+                ( err, data, response ) => {
+                    
+                    if ( err ){
+                        console.log( 'error:', err );
+                    }
+    
+                    else {
+                        console.log("Segunda Imagem Upada")
+                        ids.push(data.media_id_string);
+    
+                        Bot.post('statuses/update', {
+                            status: makeFeat(artista1, artista2),
+                            media_ids: new Array( ids )
+                        },
+                        (error, data, response) => {
+            
+                            if (error){
+                                console.log(data)
+                            }
+                            else{
+                                console.log("Feat Publicado!")
+                            }
+                        })
+            }})
+    
+        }})
+    
+    })
+})
+
+scheduler.scheduleJob({hour: 23, minute: 00 }, ()=>{
 
     let artista1 = ""
     let artista2 = ""
